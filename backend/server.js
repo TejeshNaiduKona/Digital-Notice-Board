ec6ec4b9d545cb314e16e1213cba0e8340bc446f
Tejesh Naidu <151620278+TejeshNaiduKona@users.noreply.github.com>
2026-02-05 21:15:18 +0530

    Remove unnecessary blank line in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-
 const app = express();
 
 // Middleware
